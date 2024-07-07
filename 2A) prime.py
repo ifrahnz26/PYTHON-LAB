45556cc3d15ff72af9a169528977db0bb9919001
Ifrah Naaz <120669161+ifrahnz26@users.noreply.github.com>
2024-07-07 21:05:10 +0530

    Add files via upload

---
+++ 2A) prime.py
@@ -0,0 +1,12 @@
+''' Write a python program to display all the prime numbers in the given range.'''
+
+low = int(input("Enter the lower limit: "))
+high = int(input("Enter the upper limit: "))
+while low<high+1:
+    if low>1:
+        for i in range(2,low):
+            if low%i==0:
+                break
+        else:
+            print(low)
+    low+=1
