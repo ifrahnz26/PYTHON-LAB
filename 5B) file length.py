d537a1da1f9a9576a56e0a8e4f3647e92fa6c957
Ifrah Naaz <120669161+ifrahnz26@users.noreply.github.com>
2024-08-05 11:23:58 +0530

    change code

--- 5B) file length.py
+++ 5B) file length.py
@@ -2,16 +2,17 @@
 to enter 5-6 lines of text. Display the longest 
 and the shortest word from the file. 
 Display the length of these words.'''
-
 lines = [input(f"Enter line {i+1}: ") for i in range(5)]
 f = open("file.txt", "w")
-f.writelines(lines)
+for line in lines:
+    f.write(line + "\n")
 f.close()
 f = open("file.txt", "r")
 words = f.read().split()
-words = sorted(words)
+words = sorted(words, key = lambda x: len(x))
 longest = words[-1]
 shortest = words[0]
+print(words)
 print(f"Longest word: {longest} (length: {len(longest)})")
 print(f"Shortest word: {shortest} (length: {len(shortest)})")
 f.close()
